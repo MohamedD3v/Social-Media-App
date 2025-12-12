8160518a94b34daf67905cde861d243c3399a987
Mohamed Mahmoud <mohamed.mahmoud.d3v@gmail.com>
2025-12-12 18:17:49 +0200

    Added custom Validation to Zod , Added README File

--- src/Modules/Auth/auth.validation.ts
+++ src/Modules/Auth/auth.validation.ts
@@ -1,20 +1,30 @@
 import { z } from "zod";
 
 export const signupSchema = {
-  body: z.strictObject({
-    username: z
-      .string()
-      .min(6)
-      .max(20),
-    email: z.email(),
-    password:z.string().min(8),
-    confirmPassword:z.string(),
-    phone:z.string(),
-    gender:z.string()
-  })
-// .superRefine((data,ctx)=>{
-//     if(data.confirmPassword !== data.password){
-//         ctx()
-//     }
-//   })
+  body: z
+    .strictObject({
+      username: z.string().min(6).max(20),
+      email: z.email(),
+      password: z.string().min(8),
+      confirmPassword: z.string(),
+      phone: z.string(),
+      gender: z.string(),
+    })
+    .superRefine((data, ctx) => {
+      if (data.password !== data.confirmPassword) {
+        ctx.addIssue({
+          code: "custom",
+          path: ["confirmPassword"],
+          message: "Password Not Matched !!",
+        });
+       
+      };
+      if(data.username?.split(" ").length !== 2){
+            ctx.addIssue({
+                code:"custom",
+                path:["username"],
+                message:"Username must be 2 Words , example:'Mohamed Abdellatef'"
+            })
+        }
+    }),
 };
